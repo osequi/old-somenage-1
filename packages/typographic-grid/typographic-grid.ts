fd2ff3932b0cfe59f56291f9b7518308c903b3df
osequi <cs@osequi.com>
2020-11-10 16:29:33 +0200

    Refactoring docs

--- packages/typographic-grid/typographic-grid.ts
+++ packages/typographic-grid/typographic-grid.ts
@@ -28,14 +28,6 @@ export type TTypographicGrid = {
 /**
  * Defines the typographic grid.
  *
- * Font size in percentage combined with unitless line height set on the `<body>` tag defines a typographic grid.
- *
- * For example, `font-size: 100%; line-height: 1.25` defines a grid where the cell size is '`16px * 1.25 = 20px`'.
- *
- * When child elements define their dimension and position in `em` the underlying typographic grid will stay the same across the page. This way every element aligns perfectly to the grid, both vertically and horizontally.
- *
- * The typographic grid is responsive. Setting different font sizes on the `<body>` tag with media queries makes the grid cell scale with the screen size.
- *
  * @example <caption>Responsive font sizes defined for all breakpoints:</caption>
  * fontSizes: [100, 100, 110, 120, 140], // watch, mobile, tablet, laptop, desktop
  * lineHeight: 1.25,
@@ -45,7 +37,7 @@ export type TTypographicGrid = {
  * lineHeight: 1.25,
  *
  * @category Theme
- * @see http://metamn.io/mr-ui/?selectedKind=Basics%2FTypography%20%E2%9C%93%2FClassic&selectedStory=Overview&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook-addon-background%2Fbackground-panel&background=beige
+ * @see https://bit.ly/3pew04j
  */
 const typographicGrid: TTypographicGrid = {
   fontSizes: [100, 100, 110, 120, 140],
