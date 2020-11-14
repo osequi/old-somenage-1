c8c1ec75b44c2efab55305f101f48917e58c2bed
osequi <cs@osequi.com>
2020-11-14 19:28:42 +0200

    Typographic scale added

---
+++ theme/typographic-scale/lib/typographic-scale.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Defines the typographic scale.
+ *
+ * The scale comes from one of the presets: linear scale, modular scale.
+ * @category Theme
+ * @example
+ * name: "linear",
+ * settings: {},
+ * @example
+ * name: 'modular',
+ * settings: {base:[1], ratio: 1.33}
+ */
+const typographicScale = {
+    name: "linear",
+    settings: {},
+};
+exports.default = typographicScale;
+//# sourceMappingURL=typographic-scale.js.map
---
+++ theme/typographic-scale/src/typographic-scale.ts
@@ -0,0 +1,43 @@
+import type { TLinearScale } from "@osequi/somenage-theme-linear-scale";
+import type { TModularScale } from "@osequi/somenage-theme-modular-scale";
+
+/**
+ * Defines the scale names.
+ * @category Theme
+ */
+export type TTypographicScaleNames = "linear" | "modular";
+
+/**
+ * Defines the typographic scale type.
+ * @category Theme
+ */
+export type TTypographicScale = {
+  /**
+   * The name of the scale. Identifies a scale preset.
+   */
+  name: TTypographicScaleNames;
+  /**
+   * The settings for the scale.
+   */
+  settings?: object;
+};
+
+/**
+ * Defines the typographic scale.
+ *
+ * The scale comes from one of the presets: linear scale, modular scale.
+ * @category Theme
+ * @example
+ * name: "linear",
+ * settings: {},
+ * @example
+ * name: 'modular',
+ * settings: {base:[1], ratio: 1.33}
+ */
+const typographicScale: TTypographicScale = {
+  name: "linear",
+  settings: {},
+};
+
+export default typographicScale;
+export type { TLinearScale, TModularScale };
