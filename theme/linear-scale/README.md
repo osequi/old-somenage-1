692ca0ac302f63f28d5bba75fcd0bdc104cecdb1
osequi <cs@osequi.com>
2020-11-14 19:07:55 +0200

    Linear scale added

---
+++ theme/linear-scale/README.md
@@ -0,0 +1,11 @@
+# `@osequi/somenage-theme-linear-scale`
+
+> TODO: description
+
+## Usage
+
+```
+const somenageThemeLinearScale = require('@osequi/somenage-theme-linear-scale');
+
+// TODO: DEMONSTRATE API
+```
---
+++ theme/linear-scale/lib/linear-scale.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Defines the linear scale.
+ * @category Theme
+ * @example
+ * name: "linear",
+ * settings: {},
+ */
+const linearScale = {
+    name: "linear",
+    settings: {},
+};
+exports.default = linearScale;
+//# sourceMappingURL=linear-scale.js.map
